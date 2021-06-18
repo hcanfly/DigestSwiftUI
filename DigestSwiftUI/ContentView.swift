bc4f475867da6e4df1e964932c1db3bde8667729
Gary Hanson <hcanfly@gmail.com>
2021-06-18 07:46:32 -0700

    Initial Commit

---
+++ DigestSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DigestSwiftUI
+//
+//  Created by Gary Hanson on 6/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DigestSwiftUI/DigestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigestSwiftUIApp.swift
+//  DigestSwiftUI
+//
+//  Created by Gary Hanson on 6/18/21.
+//
+
+import SwiftUI
+
+@main
+struct DigestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
